040217029401ab3745f1b5c8b9f6e2cf36e7db04
Yazhong Liu <yorkiefixer@gmail.com>
2014-07-04 21:55:58 +0800

    update

--- main.rs
+++ main.rs
@@ -1,12 +1,50 @@
 
+static BASE_NUM: int = 10;
+
+// try to define a struct in rust
+struct Point {
+  x: int,
+  y: int
+}
+
 fn main() {
-  let mut hi = 10i;
+  
+  let mut hi: int = BASE_NUM;
   loop {
     hi = hi - 1i;
     if hi == 0i {
-      return;
+      break;
     }
     println!("hi -> {}", hi);
+  }
+
+  // redefine
+  let hi = "haha";
+  println!("hi, got a string -> {}", hi);
+
+  // test match
+  let age = 14i;
+  match age*2 {
+    a @ 0..40 => println!("{} years old", a),
+    _ => println!("older than 21")
+  }
+
+  // test struct: Point
+  let mp = Point {
+    x: 1,
+    y: 10
+  };
+  
+  match inc_point(mp) {
+    p @ Point { x: 1..2, y: yy } => println!("{},{}", p.x, yy),
+    _ => println!("other!")
+  }
+}
+
+fn inc_point(p: Point) -> Point {
+  Point {
+    x: p.x + 1,
+    y: p.y + 2
   }
 }
 
